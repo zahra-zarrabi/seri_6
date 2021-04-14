cae1d3ecccbd580ae8264725dd9a7b4f3b69da6b
zahra-zarrabi <zahra.zarrabi1998@gmail.com>
2021-04-14 23:53:20 +0430

    seri_6

---
+++ Matrix.py
@@ -0,0 +1,26 @@
+import random
+n=int(input('Enter the number of columns in the matrix 1:'))
+m=int(input('Enter the number of rows in the matrix 1:'))
+k=int(input('Enter the number of columns in matrix 2:'))
+
+m1=[[random.randint(0,50)for i in range(n)]for j in range(m)]
+m2=[[random.randint(0,50)for i in range(k)]for j in range(n)]
+res=[[0 for i in range(k)]for j in range(m)]
+
+print('Matrix 1:')
+for i in range(m):
+    for j in range(n):
+        print(m1[i][j],end=' ')
+    print()
+
+print('Matrix 2:')
+for i in range(n):
+    for j in range(k):
+        print(m2[i][j],end=' ')
+    print()
+
+for i in range(m):
+    for j in range(k):
+        for z in range(n):
+            res[i][j]+=(m1[i][j]*m2[z][j])
+print('Multiply the matrix: ',res)
